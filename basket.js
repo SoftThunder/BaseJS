9eababe9252de35587168ec61461fd5918a77d01
SoftThunder <grom1304@gmail.com>
2022-04-30 22:04:17 +0300

    открытие закрытие корзины

---
+++ basket.js
@@ -0,0 +1,7 @@
+const basketOpen = document.querySelector('.cartIconWrap');
+const basketOpenMenu = document.querySelector('.basket');
+
+basketOpen.addEventListener('click', () => {
+    basketOpenMenu.classList.toggle('hidden');
+  });
+  const Basket = {};
